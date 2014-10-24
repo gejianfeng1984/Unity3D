c308e871eeced8287991b8e4b29e593e8c591f2e
Ge Jian Feng <gejianfeng2@126.com>
2014-10-24 14:52:03 +0800

    + Add Example Code

---
+++ Chapter7/Assets/code/CH07_03_GetKey.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CH07_03_GetKey : MonoBehaviour {
+
+	private int keyFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+		keyFrame = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			Debug.Log("A key pressed.");
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			keyFrame++;
+			Debug.Log("A key pressed for " + keyFrame + " frames.");
+		}
+
+		if (Input.GetKeyUp(KeyCode.A)) {
+			keyFrame = 0;
+			Debug.Log("A key released.");
+		}
+	
+	}
+}
