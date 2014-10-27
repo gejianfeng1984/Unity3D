795a95cc259c96a650304c45baee92384122a481
Ge Jian Feng <gejianfeng2@126.com>
2014-10-27 20:24:00 +0800

    + Add Example Code

---
+++ Chapter7/Assets/code/CH07_04_AnyKey.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CH07_04_AnyKey : MonoBehaviour {
+
+	int keyFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			keyFrame = 0;
+			Debug.Log("Any key is pressed.");
+		}
+
+		if (Input.anyKey) {
+			keyFrame++;
+			Debug.Log("Any key is pressed for " + keyFrame + " frames.");
+		}
+	}
+}
